4e9e8efe9861e5107f37002acd175a779fed3aa9
FernandoAurelius <miguelfernandoaurelius@gmail.com>
2025-11-12 18:00:50 -0300

    config: adiciona configurações e bootstrap inicial do Amplify

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import { Amplify } from 'aws-amplify'
+import outputs from '../amplify_outputs.json'
 
 import '@/assets/main.css'
 
+Amplify.configure(outputs)
 createApp(App).mount('#app')
