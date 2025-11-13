f957bb24b5ad552eaa01eadd802e62a67f380097
FernandoAurelius <miguelfernandoaurelius@gmail.com>
2025-11-12 21:25:59 -0300

    feat(data): adiciona funções básicas de API em cliente centralizado

---
+++ src/lib/api.ts
@@ -0,0 +1,25 @@
+import { generateClient } from "aws-amplify/api";
+import type { Schema } from '../../amplify/data/resource';
+
+type Message = Schema['Message'];
+
+const client = generateClient<Schema>();
+
+export async function getMessages() {
+    const response = await client.models.Message.list();
+    return response.data;
+}
+
+export async function createMessage(text: string, authorEmail: string) {
+    await client.models.Message.create({
+        text,
+        authorEmail,
+    });
+}
+
+export async function likeMessage(id: string, currentLikes: number) {
+    await client.models.Message.update({
+        id,
+        likes: currentLikes + 1,
+    });
+}
